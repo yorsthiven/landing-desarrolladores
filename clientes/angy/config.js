d9cc05fa310f775832f76c5750ec5bbe31a34bb3
stiven medina <69610934+yorsthiven@users.noreply.github.com>
2026-03-22 16:29:20 -0500

    nuevo proyecto angy
    agregar otro ejemplo de pagina

---
+++ clientes/angy/config.js
@@ -0,0 +1,43 @@
+const clienteConfig = {
+    nombre: "AngyFisio | fISIOTERAPIA Y REHABILITACIÓN",
+    
+    // Colores del cliente
+    colores: {
+        primario: "#3b82f6",           // Azul principal
+        secundario: "#d6b7b7",          // Verde esmeralda
+        letraGrande: "#000000",         // Blanco para títulos
+        letrasChicas: "#090a0a",        // Gris claro para texto pequeño
+        spanDestacado: "#f1b6bb",       // Violeta para span entre título
+        fondoBanner: "#2aa5f7",         // Fondo oscuro del banner
+        botonServicio: "#3b82f6",       // Azul para botones de servicios
+        navFondo: "#70737a52",   // Fondo del nav con transparencia
+        navTexto: "#000000"             // Color de texto del nav
+    },
+    
+    logo: "logo.webp",
+    favicon: "favicon_lineasclaras.ico",
+    banner: "banner.jpg",
+    heroTitle: "Ergonomía de Vanguardia para",
+    heroTitle2: "La Salud y Bienestar de los trabajadores",
+    heroDesc: "Especialista en gerencia de seguridad y salud en el trabajao, con amplia experiencia en el área.",
+    metaDescription: "Ingeniero de Software especializado en automatización y soluciones digitales de alto impacto.",
+    metaKeywords: "Desarrollador de Software, Automatización, WorkScan, .NET, Angular",
+    whatsapp: "3168874293",
+    footerInfo: "&copy; 2026 Yors Dev | Desarrollado con el stack más eficiente del mercado.",
+    servicios: [
+        {
+            categoria: "Administración",
+            titulo: "Inspecciones de puestos de trabajo",
+            desc: "Verificación de condiciones eronomicas de puestos de trabajo en áreas de administrativas",
+            tags: ["Inspecciónes", "Ergonomía", "Bienestar"],
+            estilo: "blue"
+        },
+        {
+            categoria: "Administración",
+            titulo: "Inspecciones de puestos de trabajo",
+            desc: "Verificación de condiciones eronomicas de puestos de trabajo en áreas de administrativas",
+            tags: ["Inspecciónes", "Ergonomía", "Bienestar"],
+            estilo: "blue"
+        }
+    ]
+};
